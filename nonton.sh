2a2a51c3e97a0c9a700d0d5cc773060d4a95ef61
NIXY Project <152050497+nixyproject-cfd@users.noreply.github.com>
2024-01-08 09:45:15 +0700

    NIXY STORE

--- nonton.sh
+++ nonton.sh
@@ -13,7 +13,7 @@ Font_Suffix="\033[0m";
 
 
 clear;
-echo -e "  \033[1;37m${Font_Purple}Media Stream Unlocker Test Mod By PelangiSenja${Font_Suffix}\033[0m";
+echo -e "  \033[1;37m${Font_Purple}Media Stream Unlocker Test Rec By Dyzzy project FT Nixy project${Font_Suffix}\033[0m";
 echo -e "  \033[1;37mVersion : ${Font_SkyBlue}${shell_version}${Font_Suffix}\033[0m";
 echo -e "  \033[1;37mTime    : $(date)\033[0m"
 
